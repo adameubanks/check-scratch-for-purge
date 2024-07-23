ef930fe20c600074613a7b1bffd369c324409f04
KhanradCoder <akhanrade@gmail.com>
2024-07-23 11:11:46 -0400

    add download output

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'sinatra'
 require 'erubi'
 require './command'
 
@@ -10,9 +11,30 @@ end
 
 # Define a route at the root '/' of the app.
 get '/' do
-  @command = Command.new
-  @processes, @error = @command.exec
+  erb :index, locals: { display_table: false, processes: nil }
+end
 
-  # Render the view
-  erb :index
+# Define a route to handle form submission.
+post '/process_form' do
+  write_to_file = params['writeToFile'] == 'yes'
+  file_location = params['fileLocation']
+
+  command = Command.new
+  processes, error = command.exec
+
+  if write_to_file
+    # Determine the file path based on user selection.
+    file_path = file_location == 'home' ? '~/output.txt' : '/scratch/output.txt'
+
+    # Write the output to the specified file.
+    File.open(File.expand_path(file_path), 'w') do |file|
+      processes.each { |line| file.puts(line) }
+    end
+
+    # Redirect back to the form page or to a success page.
+    redirect '/'
+  else
+    # Render the index view with the command output for display in the table.
+    erb :index, locals: { display_table: true, processes: processes }
+  end
 end
