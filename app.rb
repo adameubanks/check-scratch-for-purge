5175469bafeb6f575c9a2f74b308e853bfed2764
KhanradCoder <akhanrade@gmail.com>
2024-07-31 11:26:52 -0400

    fix routing error

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require 'json'
 
 set :erb, :escape_html => true
 
-# purge_command = "find /scratch/$USER -type f -atime +90 -ctime +90 -mtime +90 -print"
-purge_command = "ls"
+purge_command = "find /scratch/$USER -type f -atime +90 -ctime +90 -mtime +90 -print"
+# purge_command = "ls"
 
 if development?
   require 'sinatra/reloader'
@@ -18,15 +18,14 @@ get '/' do
   output, error = command.exec(purge_command)
   @output = command.parse(output)
   @error = error
-  erb :index, locals: { output: @output, error: @error }
+  erb :index
 end
 
 get '/downloadToScratch' do
   command = Command.new
   output, error = command.exec("#{purge_command} > /scratch/$USER/scratchFilesToBePurged.txt")
-  @output = command.parse(output)
-  @error = error
-  erb :index, locals: { output: @output, error: @error }
+  content_type :json
+  { output: command.parse(output), error: error }.to_json
 end
 
 get '/downloadToHome' do
@@ -35,13 +34,4 @@ get '/downloadToHome' do
   @output = command.parse(output)
   @error = error
   erb :index, locals: { output: @output, error: @error }
-end
-
-get '/downloadLocally' do
-  command = Command.new
-  output, error = command.exec(purge_command)
-  @output = command.parse(output)
-  @error = error
-  content_type :json
-  { output: @output.join('\n'), error: @error }.to_json
 end
